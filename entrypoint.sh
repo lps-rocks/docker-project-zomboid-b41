04e1e26181d92314a43b60107c1fbcd12af8dfa4
lps-rocks <admin@lps.rocks>
2022-10-17 23:29:20 -0400

    trap and safely quit server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Exit functions
+function stop_server() {
+	tmux send-keys -t 0 "save" Enter "quit" Enter 
+} 
+
+# Trap 
+trap stop_server SIGTERM SIGINT SIGQUIT SIGHUP ERR
+
 # Set the user ID and group id
 usermod -u ${PUID} steam
 groupmod -g ${PGID} steam 
@@ -11,7 +19,10 @@ chown -Rf steam:steam /home/steam
 sudo -u steam /usr/games/steamcmd "+force_install_dir /home/steam/pzserver +login anonymous +app_update ${STEAM_APP} validate +quit"
 
 # Launch server 
-sudo -u steam /home/steam/pzserver/start-server.sh -servername ${SERVER_NAME} ${SERVER_ADDITIONAL_PARAMS}
+sudo -u steam tmux new-session -d /home/steam/pzserver/start-server.sh -servername ${SERVER_NAME} ${SERVER_ADDITIONAL_PARAMS}
 
-# Return exit code of server
-exit $?
+# Wait for server to exit
+sudo -u steam tmux wait-for 0 
+
+# Exit
+exit 0 
